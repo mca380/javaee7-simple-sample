f86aef44391cccd33634139a9b3f0c60864e832c
Marina Cánovas Amer <mcanovas@brujula.es>
2018-03-13 12:16:29 +0100

    login y clase user

---
+++ src/main/java/bru/rrhh/candidates/MAIN.java
@@ -0,0 +1,8 @@
+package bru.rrhh.candidates;
+
+/**
+ * Created by mcanovas on 13/03/2018.
+ */
+public class MAIN {
+
+}
---
+++ src/main/java/bru/rrhh/candidates/user.java
@@ -0,0 +1,27 @@
+package bru.rrhh.candidates;
+
+/**
+ * Created by mcanovas on 13/03/2018.
+ */
+public class user {
+
+        private String Usuario;
+
+        private String Clave;
+
+    public String getUsuario(){
+        return Usuario;
+    }
+    public void setUsuario(String usuario) {
+        Usuario = usuario;
+    }
+
+    public String getClave() {
+        return Clave;
+    }
+
+    public void setClave(String clave) {
+        Clave = clave;
+    }
+
+}
